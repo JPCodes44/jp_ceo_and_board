e9916b6edaf4e2a9946d3a56d4fbaad336861e5c
JPCodes44 <j29mak@uwaterloo.ca>
2026-03-31 17:15:29 -0400

    changed the function declaration of my update-ui extension

--- .pi/extensions/update-ui.ts
+++ .pi/extensions/update-ui.ts
@@ -1,10 +1,10 @@
 import type { ExtensionAPI } from '@mariozechner/pi-coding-agent';
 
-export const updateUI = (pi: ExtensionAPI) => {
+export default function (pi: ExtensionAPI) {
   pi.registerCommand('update UI', {
     description: 'say hello',
     handler: async (_args, ctx) => {
       ctx.ui.notify('Hello!');
     },
   });
-};
+}
