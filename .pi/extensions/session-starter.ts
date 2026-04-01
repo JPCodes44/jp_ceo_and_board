56aa66e41809935c2381050c77a032e1fa0ccaae
JPCodes44 <j29mak@uwaterloo.ca>
2026-04-01 13:56:46 -0400

    removed the test session starter extension

--- .pi/extensions/session-starter.ts
+++
@@ -1,7 +0,0 @@
-import type { ExtensionAPI } from '@mariozechner/pi-coding-agent';
-
-export default function (pi: ExtensionAPI) {
-  pi.on('session_start', async (_event, ctx) => {
-    ctx.ui.notify('session started');
-  });
-}
